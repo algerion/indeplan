ec4370e1c515351fce0e98acb83ab77b967dd9b6
algerion <orctek-varios@yahoo.com.mx>
2015-11-04 13:11:46 -0600

    Cambio de conexion a dbcon
    Modificación de las instrucciones que usaban la clase conexion para que
    usen dbcon

--- protected/pages/Captura.php
+++ protected/pages/Captura.php
@@ -1,17 +1,15 @@
 <?php
-include_once('../compartidos/clases/conexion.php');
-include_once('../compartidos/clases/listas.php');
+include_once('../compartidos/clases/DbCon.php');
 
 class Captura extends TPage
 {
-	var $dbConexion;
+	var $cn;
 
 	public function onLoad($param)
 	{
 		parent::onLoad($param);
 
-		$this->dbConexion = Conexion::getConexion($this->Application, "db");
-		Conexion::createConfiguracion();
+		$this->cn = new DbCon($this, "db");
 
 		if(!$this->IsPostBack)
 		{
@@ -22,32 +20,30 @@ class Captura extends TPage
 	public function ddls()
 	{
 		$this->enlazaCG();
-		//$this->enlazaProgramas();
-		//$this->enlazaMedios();
 	}
 
 	public function enlazaCG($sender = null, $param = null)
 	{
-		Listas::EnlazaLista($this->dbConexion, "SELECT DISTINCT cg.id_cg, CONCAT(cg.clave, '. ', cg.denominacion) AS cg FROM cg JOIN cg_programas_indicadores cpi on cg.id_cg = cpi.id_cg ORDER BY clave", $this->ddlCG);
+		$this->cn->enlaza($this->ddlCG, "SELECT DISTINCT cg.id_cg, CONCAT(cg.clave, '. ', cg.denominacion) AS cg FROM cg JOIN cg_programas_indicadores cpi on cg.id_cg = cpi.id_cg ORDER BY clave");
 		$this->ddlCG->raiseEvent("OnSelectedIndexChanged", $this->ddlCG, new TCallbackEventParameter(null, null));
 	}
 
 	public function enlazaProgramas($sender = null, $param = null)
 	{
-		Listas::EnlazaLista($this->dbConexion, "SELECT DISTINCT p.id_programa, p.programa FROM programas p JOIN cg_programas_indicadores cpi on p.id_programa = cpi.id_programa WHERE cpi.id_cg = " . $this->ddlCG->SelectedValue, $this->ddlPrograma);
+		$this->cn->enlaza($this->ddlPrograma, "SELECT DISTINCT p.id_programa, p.programa FROM programas p JOIN cg_programas_indicadores cpi on p.id_programa = cpi.id_programa WHERE cpi.id_cg = :id_cg", array("id_cg"=>$this->ddlCG->SelectedValue));
 		$this->ddlPrograma->raiseEvent("OnSelectedIndexChanged", $this->ddlPrograma, null);
 	}
 
 	public function enlazaddlUnidades($sender = null, $param = null)
 	{
-		Listas::EnlazaLista($this->dbConexion, "SELECT i.id_indicador, indicador FROM indicadores i JOIN cg_programas_indicadores cpi ON i.id_indicador = cpi.id_indicador WHERE cpi.id_programa = " . $this->ddlPrograma->SelectedValue . " AND cpi.id_cg = " . $this->ddlCG->SelectedValue, $this->ddlIndicador);
+		$this->cn->enlaza($this->ddlIndicador, "SELECT i.id_indicador, indicador FROM indicadores i JOIN cg_programas_indicadores cpi ON i.id_indicador = cpi.id_indicador WHERE cpi.id_programa = :id_programa AND cpi.id_cg = :id_cg", array("id_programa"=>$this->ddlPrograma->SelectedValue, "id_cg"=>$this->ddlCG->SelectedValue));
 		$this->ddlIndicador->raiseEvent("OnSelectedIndexChanged", $this->ddlIndicador, null);
 	}
 	
 	public function escribeUnidad($sender = null, $param = null)
 	{
-		$indicador = Conexion::Retorna_Consulta($this->dbConexion, "indicadores", array("unidad", "meta"), array("id_indicador"=>$this->ddlIndicador->SelectedValue));
-		$nums = Conexion::Retorna_Consulta($this->dbConexion, "cg_programas_indicadores", array("meta_num", "avance"), array("id_cg"=>$this->ddlCG->SelectedValue, "id_programa"=>$this->ddlPrograma->SelectedValue, "id_indicador"=>$this->ddlIndicador->SelectedValue));
+		$indicador = $this->cn->consulta("SELECT unidad, meta FROM indicadores WHERE id_indicador = :id_indicador", array("id_indicador"=>$this->ddlIndicador->SelectedValue));
+		$nums = $this->cn->consulta("SELECT meta_num, avance FROM cg_programas_indicadores WHERE id_cg = :id_cg AND id_programa = :id_programa AND id_indicador = :id_indicador", array("id_cg"=>$this->ddlCG->SelectedValue, "id_programa"=>$this->ddlPrograma->SelectedValue, "id_indicador"=>$this->ddlIndicador->SelectedValue));
 		$this->lblUnidad->Text = $indicador[0]["unidad"];
 		$meta_num = $nums[0]["meta_num"];
 		$this->txtAvance->Text = $nums[0]["avance"];
@@ -62,7 +58,7 @@ class Captura extends TPage
 				"id_programa"=>$this->ddlPrograma->SelectedValue,
 				"id_indicador"=>$this->ddlIndicador->SelectedValue
 				);
-		Conexion::Actualiza_Registro($this->dbConexion, "cg_programas_indicadores", $parametros, $busqueda);
+		$this->cn->actualiza("cg_programas_indicadores", $parametros, $busqueda);
 		$this->getClientScript()->registerBeginScript("guardado",
 				"alert('Indicador actualizado');\n"
 //				. "document.location.href = 'index.php?page=Captura';\n"
